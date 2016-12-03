65e82309ca06e2c69c6b656071246c6e7da7a8cb
abhishek97 <zapper.awake@gmail.com>
2016-12-03 19:39:38 +0530

    Increase Event Listener

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const fs = require('fs');
 const stream = fs.createWriteStream('var/log/natlog', { 'flags': 'a'
     , 'encoding': 'utf-8'
 });
+const emitter = new EventEmitter();
+emitter.setMaxListeners(1000);
 //const netmask = require('netmask').Netmask;
 
 /*
